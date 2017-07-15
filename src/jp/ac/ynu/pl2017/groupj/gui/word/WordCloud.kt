91ebfaa0d6a2089685e8e0e394d7f20a0e3617d6
yuta-takanashi <takanashi-yuta-rs@ynu.jp>
2017-07-16 00:39:07 +0900

    掃除

--- src/jp/ac/ynu/pl2017/groupj/gui/word/WordCloud.kt
+++ src/jp/ac/ynu/pl2017/groupj/gui/word/WordCloud.kt
@@ -2,18 +2,15 @@ package jp.ac.ynu.pl2017.groupj.gui.word
 
 import javafx.fxml.FXML
 import javafx.fxml.Initializable
-import javafx.scene.image.Image
 import javafx.scene.image.ImageView
 import jp.ac.ynu.pl2017.groupj.net.HaikookingConnection
-import jp.ac.ynu.pl2017.groupj.util.ConnectionCommand
-import jp.ac.ynu.pl2017.groupj.util.Season
 import java.net.URL
 import java.util.*
 
 /**
  * WordCloudの表示画面のコントローラー。この画面はモーダルで表示する。
  */
-class WordCloud(/*val images: Array<Image>*/) : Initializable {
+class WordCloud() : Initializable {
     @FXML lateinit var total: ImageView
     @FXML lateinit var week: ImageView
     @FXML lateinit var month: ImageView
