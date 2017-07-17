2fb853357cff013b8077ae6612ff18c63f145385
FUJITSU-PC\user <gempei@outlook.jp>
2017-07-17 21:49:13 +0900

    7/17 画像全体縁取りとコンポーネントの重なり防止はまだです。いままでのcreateHaikuImageメソッドの最後の引数にString型"spring","summer","autumn","winter"のいずれかを入力するとその季節っぽい形に画像を切り抜きます(現状各季節1種ずつ)。

--- src/jp/ac/ynu/pl2017/groupj/util/ImageUtil.java
+++ src/jp/ac/ynu/pl2017/groupj/util/ImageUtil.java
@@ -12,6 +12,7 @@ import java.awt.image.ColorConvertOp;
 import java.awt.image.MemoryImageSource;
 import java.awt.image.PixelGrabber;
 import java.io.File;
+import java.util.HashMap;
 import java.util.Random;
 
 import javax.imageio.ImageIO;
@@ -29,6 +30,35 @@ public class ImageUtil {
 	
 	private static Image haikuimage=null;
 	private static Image haikuimageWithStr=null;
+	private static String pathToShapeimg="res/image/shapedimage";
+	private static File[] shapeimgfile=(new File(pathToShapeimg)).listFiles();
+	private static int shapenum=shapeimgfile.length;
+	private static int springshapenum=0;
+	private static int summershapenum=0;
+	private static int autumnshapenum=0;
+	private static int wintershapenum=0;
+	private static HashMap<String,BufferedImage> shapeimgmap=new HashMap<String,BufferedImage>(){
+		{
+			for(int i=0;i<shapenum;i++){
+				try{
+					BufferedImage img=ImageIO.read(shapeimgfile[i]);
+					String filename=shapeimgfile[i].getName().substring(0,shapeimgfile[i].getName().indexOf(".")); 
+					if(filename.startsWith("spring")){
+						springshapenum++;
+					}else if(filename.startsWith("summer")){
+						summershapenum++;
+					}else if(filename.startsWith("autumn")){
+						autumnshapenum++;
+					}else if(filename.startsWith("winter")){
+						wintershapenum++;
+					}
+					put(filename,img);
+				}catch(Exception e){
+					e.printStackTrace();
+				}
+			}
+    	}
+	};
 	///*
 		public static void main(String[] args) {
 			// TODO 自動生成されたメソッド・スタブ
@@ -56,11 +86,11 @@ public class ImageUtil {
 			Image wcView=SwingFXUtils.toFXImage(wctest,null);
 			Image backimagefx=SwingFXUtils.toFXImage(backimage,null);
 			java.awt.Image wcs=WCImagemodify(wcView,backimagefx);
-			String[] Haiku={"古池や","蛙飛び込む","水の音"};
+			String[] Haiku={"古池や","かえる飛び込む","水の音"};
 			Image Haikuimg=null;
-			createHaikuImage(150,backimagefx,fximg,Haiku);
-			//synimg=SwingFXUtils.fromFXImage(gethaikuimgWithstr(), null);//俳句付き画像
-			synimg=SwingFXUtils.fromFXImage(getHaikuimg(), null); //俳句なし画像表示テスト用
+			createHaikuImage(150,backimagefx,fximg,Haiku,"spring");
+			synimg=SwingFXUtils.fromFXImage(gethaikuimgWithstr(), null);//俳句付き画像
+			//synimg=SwingFXUtils.fromFXImage(getHaikuimg(), null); //俳句なし画像表示テスト用
 			
 			JLabel icon=new JLabel(new ImageIcon((java.awt.Image)synimg));
 			//JLabel icon=new JLabel(new ImageIcon(wcs));
@@ -220,6 +250,7 @@ public class ImageUtil {
 			}
 			//Image c=(Image)Bcircle;
 			Bcircle=magnifySizeImage(Bcircle,width,height);
+			Bcircle=getBWImage(Bcircle);
 			java.awt.Image c=(java.awt.Image)Bcircle;
 			int[] circlepixel = new int[width*height];
 			PixelGrabber cpg = new PixelGrabber(c,0,0,width,height,circlepixel,0,width);
@@ -231,6 +262,45 @@ public class ImageUtil {
 
 			for(int i=0;i<width*height;i++){
 				if(circlepixel[i]==-1){
+					pixel[i]=pixel[i]& 0x00FFFFFF;
+				}
+			}
+			java.awt.Image alphaImage = Toolkit.getDefaultToolkit().createImage(new MemoryImageSource(width, height, pixel, 0, width));
+			return alphaImage;
+
+
+			}
+		
+		private static java.awt.Image ShapedImage(BufferedImage img,BufferedImage shape){
+			//imgが切り抜く素材画像,shapeが切り抜く形の画像
+			if(img==null||shape==null) return null;
+			int width = img.getWidth(null);
+			int height = img.getHeight(null);
+			BufferedImage shapedimg=shape;
+
+			//ピクセル値取得
+			int[] pixel = new int[width*height];
+			PixelGrabber pg = new PixelGrabber(img,0,0,width,height,pixel,0,width);
+			try {
+				pg.grabPixels();
+			} catch (InterruptedException e) {
+				return null;
+			}
+
+			//Image c=(Image)Bcircle;
+			shapedimg=magnifySizeImage(shapedimg,width,height);
+			shapedimg=getBWImage(shapedimg);
+			java.awt.Image c=(java.awt.Image)shapedimg;
+			int[] shapepixel = new int[width*height];
+			PixelGrabber cpg = new PixelGrabber(c,0,0,width,height,shapepixel,0,width);
+			try {
+				cpg.grabPixels();
+			} catch (InterruptedException e) {
+				return null;
+			}
+
+			for(int i=0;i<width*height;i++){
+				if(shapepixel[i]==-1){
 					pixel[i]=pixel[i]& 0x00FFFFFF;
 				}
 			}
@@ -288,7 +358,7 @@ public class ImageUtil {
 		}
 		
 		public static void createHaikuImage(int compsize,Image backimage,Image[] img,String[] str){
-			//2種の俳句画像を生成、compsizeは合成素材画像サイズ,backimageは背景画像,strは俳句をいれる
+			//2種の俳句画像を生成、compsizeは合成素材正方画像サイズ,backimageは背景画像,strは俳句をいれる
 			if(backimage==null || img.length==0 || str.length!=3) return;
 			BufferedImage newbackimage=null;
 			resetHaikuImage();
@@ -304,7 +374,14 @@ public class ImageUtil {
 				BufferedImage bimg=SwingFXUtils.fromFXImage(img[i], null);
 				bimg=magnifySizeImage(bimg,compsize,compsize);//コンポーネント画像加工
 				bimg=getgrayImage(bimg);
-				java.awt.Image afterimg=CircleImage(bimg);
+				//java.awt.Image afterimg=CircleImage(bimg);
+				BufferedImage shapedimage=null;
+				try{
+				shapedimage=shapeimgmap.get("circle");
+				}catch(Exception e){
+					e.printStackTrace();
+				}
+				java.awt.Image afterimg=ShapedImage(bimg,shapedimage);
 			
 				int x=rnd.nextInt(450-compsize); //座標のランダム生成
 				int y=rnd.nextInt(450-compsize);
@@ -316,6 +393,7 @@ public class ImageUtil {
 			//Image Haikuimg=SwingFXUtils.toFXImage(synimg, null);
 			haikuimage=SwingFXUtils.toFXImage(synimg, null);
 			synimg=DrawString(synimg,str);
+			synimg=createBufferedImage(FontFramed((java.awt.Image)synimg));
 			haikuimageWithStr=SwingFXUtils.toFXImage(synimg, null);
 			//return Haikuimg;
 			
@@ -323,7 +401,59 @@ public class ImageUtil {
 			
 		}
 		
-		private static int getHaikukey(String[] str){
+		public static void createHaikuImage(int compsize,Image backimage,Image[] img,String[] str,String season){
+			//2種の俳句画像を生成、compsizeは合成素材正方画像サイズ,backimageは背景画像,strは俳句をいれる
+			if(backimage==null || img.length==0 || str.length!=3) return;
+			BufferedImage newbackimage=null;
+			resetHaikuImage();
+			
+			newbackimage=SwingFXUtils.fromFXImage(backimage,null);
+			newbackimage=magnifySizeImage(newbackimage,450,450);//背景画像加工 //450*450
+			BufferedImage synimg=newbackimage;
+			Random rnd = new Random(getHaikukey(str));
+			int seasonmaxnum=0;
+			
+			if(season=="spring") seasonmaxnum=springshapenum;
+			else if(season=="summer") seasonmaxnum=summershapenum;
+			else if(season=="autumn") seasonmaxnum=autumnshapenum;
+			else if(season=="winter") seasonmaxnum=wintershapenum;
+			
+			if(seasonmaxnum!=0){
+			for(int i=0;i<img.length;i++){
+				int randomnum=rnd.nextInt(seasonmaxnum);
+				StringBuffer seasonkey=new StringBuffer(season);
+				seasonkey.append(Integer.toString(randomnum+1));
+				System.out.println(seasonkey);
+				BufferedImage bimg=SwingFXUtils.fromFXImage(img[i], null);
+				bimg=magnifySizeImage(bimg,compsize,compsize);//コンポーネント画像加工
+				bimg=getgrayImage(bimg);
+				//java.awt.Image afterimg=CircleImage(bimg);
+				BufferedImage shapedimage=null;
+				try{
+					shapedimage=shapeimgmap.get(seasonkey.toString());
+				}catch(Exception e){
+					e.printStackTrace();
+				}
+				java.awt.Image afterimg=ShapedImage(bimg,shapedimage);
+			
+				int x=rnd.nextInt(450-compsize); //座標のランダム生成
+				int y=rnd.nextInt(450-compsize);
+			
+				synimg=synthesizeImage(synimg,afterimg,x,y); //合成
+				}
+			
+			haikuimage=SwingFXUtils.toFXImage(synimg, null);
+			synimg=DrawString(synimg,str);
+			synimg=createBufferedImage(FontFramed((java.awt.Image)synimg));
+			haikuimageWithStr=SwingFXUtils.toFXImage(synimg, null);
+			}else{
+				createHaikuImage(compsize,backimage,img,str);
+			}
+			
+			
+		}
+		
+		private static int getHaikukey(String[] str){//俳句に応じたkeyを返す(乱数シード用)
 			int key=0;
 			for(int i=0;i<str.length;i++){
 				for(int j=0;j<str[i].length();j++){
@@ -405,8 +535,8 @@ public class ImageUtil {
 				return null;
 			}
 			
-			for(int i=0;i<width*height;i++){//白透過
-				bpixel[i]=pixel[i]&0xFFFFFFFF;
+			for(int i=0;i<width*height;i++){
+				bpixel[i]=bpixel[i]&0x3FFFFFFF;
 			}
 			java.awt.Image backalphaImage = Toolkit.getDefaultToolkit().createImage(new MemoryImageSource(width, height, bpixel, 0, width));
 			
@@ -417,23 +547,104 @@ public class ImageUtil {
 			
 		}
 		
-
-		    public static int a(int c){
+		private static java.awt.Image FontFramed(java.awt.Image img){
+			int width = img.getWidth(null);
+			int height = img.getHeight(null);
+			int[] pixel = new int[width*height];
+			PixelGrabber pg = new PixelGrabber(img,0,0,width,height,pixel,0,width);
+			try {
+				pg.grabPixels();
+			} catch (InterruptedException e) {
+				return null;
+			}
+			
+			int blackRGB=Color.BLACK.getRGB();
+			int whiteRGB=Color.WHITE.getRGB();
+			
+			for(int i=0;i<width*height;i++){//白透過
+				if(pixel[i]==blackRGB){
+					if(i+height*3<height*width)	{
+						for(int j=1;j<4;j++){
+							if(pixel[i+height*j]!=blackRGB&&i+height*j<=width*height)	pixel[i+height*j]=whiteRGB;
+							if(pixel[i-height*j]!=blackRGB&&i-height*j>=width*height)	pixel[i-height*j]=whiteRGB;
+							if(pixel[i-1*j]!=blackRGB&&i+1*j<=width*height)	pixel[i-1*j]=whiteRGB;
+							if(pixel[i+1*j]!=blackRGB&&i-1*j>=width*height)	pixel[i+1*j]=whiteRGB;
+							if(pixel[i+height+1]!=blackRGB&&i+height+1<=width*height)	pixel[i+height+1]=whiteRGB;
+							if(pixel[i+height-1]!=blackRGB&&i+height-1<=width*height)	pixel[i+height-1]=whiteRGB;
+							if(pixel[i-height+1]!=blackRGB&&i-height+1>=width*height)	pixel[i-height+1]=whiteRGB;
+							if(pixel[i-height-1]!=blackRGB&&i-height+1>=width*height)	pixel[i-height-1]=whiteRGB;
+							}
+						}
+				}
+			}
+			java.awt.Image alphaImage = Toolkit.getDefaultToolkit().createImage(new MemoryImageSource(width, height, pixel, 0, width));
+			return alphaImage;
+		}
+		
+		
+		private static int[][] createXYpos(int key,BufferedImage compimg[],int num,int xmax,int ymax){
+		//配置コンポーネントがある程度重複しないような乱数生成メソッド
+			if(compimg.length==0||num==0||xmax==0||ymax==0) return null;
+			int overlappixel=0; //重なるピクセルの許可数
+			int failcount=0; //連続乱数失敗回数
+			int[][] xypos=new int[num][2];//返す座標値
+			
+			for(int i=0;i<xypos.length;i++){//配列初期化
+				for(int j=0;j<2;j++) xypos[i][j]=0;
+			}
+			
+			//以下真っ白画像作成
+			int whiteRGB=Color.WHITE.getRGB();
+			int[] pixel = new int[xmax*ymax];
+			for(int i=0;i<xmax*ymax;i++){
+				pixel[i]=whiteRGB;
+			}
+			
+			java.awt.Image whiteimage = Toolkit.getDefaultToolkit().createImage(new MemoryImageSource(xmax, ymax, pixel, 0, xmax));
+			
+			
+			Random rnd = new Random(key);
+			BufferedImage bimg;
+			java.awt.Image newimage=whiteimage;
+			
+			for(int i=0;i<num;i++){
+				bimg=compimg[i];
+				int width = bimg.getWidth(null);
+				int height = bimg.getHeight(null);
+			while(failcount<20){
+				int x=rnd.nextInt(xmax-width); 
+				int y=rnd.nextInt(ymax-height);
+				if(pixel[x+y*ymax]==whiteRGB){
+					newimage=synthesizeImage(whiteimage,compimg[0],x,y);
+					xypos[i][0]=x;
+					xypos[i][1]=y;
+				}else{
+					failcount++;
+				}
+			}
+			}
+			
+			return xypos;
+		}
+		
+		
+			//以下は引数からアルファ値及びRGBのそれぞれの値を抽出するメソッド
+		    private static int a(int c){
 		        return c>>>24;
 		    }
-		    public static int r(int c){
+		    private static int r(int c){
 		        return c>>16&0xff;
 		    }
-		    public static int g(int c){
+		    private static int g(int c){
 		        return c>>8&0xff;
 		    }
-		    public static int b(int c){
+		    private static int b(int c){
 		        return c&0xff;
 		    }
-		    public static int rgb(int r,int g,int b){
+		    private static int rgb(int r,int g,int b){
 		        return 0xff000000 | r <<16 | g <<8 | b;
 		    }
-		    public static int argb(int a,int r,int g,int b){
+		    private static int argb(int a,int r,int g,int b){
 		        return a<<24 | r <<16 | g <<8 | b;
 		    }
 
