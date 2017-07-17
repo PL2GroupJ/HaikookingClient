d321e747decfb28c0d265682fa5226d7af7e245a
yuta-takanashi <takanashi-yuta-rs@ynu.jp>
2017-07-17 14:42:16 +0900

    自サーバに接続出来ない時の処理を追加

--- src/jp/ac/ynu/pl2017/groupj/gui/word/WordCloud.kt
+++ src/jp/ac/ynu/pl2017/groupj/gui/word/WordCloud.kt
@@ -1,19 +1,24 @@
 package jp.ac.ynu.pl2017.groupj.gui.word
 
+import javafx.application.Platform
 import javafx.concurrent.Task
 import javafx.fxml.FXML
 import javafx.fxml.Initializable
+import javafx.scene.control.Alert
 import javafx.scene.image.Image
 import javafx.scene.image.ImageView
+import jp.ac.ynu.pl2017.groupj.gui.HidePane
 import jp.ac.ynu.pl2017.groupj.net.HaikookingConnection
 import java.net.URL
 import java.util.*
 import java.util.concurrent.Executors
+import kotlin.concurrent.thread
 
 /**
  * WordCloudの表示画面のコントローラー。この画面はモーダルで表示する。
  */
-class WordCloud : Initializable {
+class WordCloud : Initializable, HidePane {
+    override lateinit var hide: () -> Unit
     @FXML lateinit var total: ImageView
     @FXML lateinit var week: ImageView
     @FXML lateinit var month: ImageView
@@ -26,7 +31,20 @@ class WordCloud : Initializable {
     override fun initialize(location: URL?, resources: ResourceBundle?) {
         val imageViews = arrayOf(total, week, month, spring, summer, autumn, winter, newYear)
         val con = HaikookingConnection()
-        if (!con.openConnection()) return
+        if (!con.openConnection()) {
+            Alert(Alert.AlertType.INFORMATION).apply {
+                title = "インターネット接続がありません"
+                headerText = null
+                contentText = "サーバーに接続できませんでした。${System.lineSeparator()}インターネット接続環境を確認後、もう一度お試しください。"
+                show()
+            }
+            // hideが初期化されるのを待つために、スレッドで少し遅延させる。UIスレッドで動かす必要がある
+            Platform.runLater {
+                Thread.sleep(100)
+                hide()
+            }
+            return
+        }
 
         val task = object : Task<Array<Image>>() {
             override fun call(): Array<Image> = con.readImages()
