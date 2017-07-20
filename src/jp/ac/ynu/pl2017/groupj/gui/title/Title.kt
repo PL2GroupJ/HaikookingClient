f5a9549fad9adf8be2c4a17df179dfb0d52f4743
yuta-takanashi <takanashi-yuta-rs@ynu.jp>
2017-07-21 01:46:41 +0900

    タイトル画像の追加。エンターでスタートできるように

--- src/jp/ac/ynu/pl2017/groupj/gui/title/Title.kt
+++ src/jp/ac/ynu/pl2017/groupj/gui/title/Title.kt
@@ -3,8 +3,10 @@ package jp.ac.ynu.pl2017.groupj.gui.title
 import javafx.fxml.FXML
 import javafx.fxml.Initializable
 import javafx.scene.canvas.Canvas
+import javafx.scene.image.ImageView
 import jp.ac.ynu.pl2017.groupj.gui.TransitionPane
 import jp.ac.ynu.pl2017.groupj.gui.haiku.Haiku
+import jp.ac.ynu.pl2017.groupj.util.getResourceAsImage
 import java.net.URL
 import java.util.*
 
@@ -13,10 +15,12 @@ import java.util.*
  */
 class Title : Initializable, TransitionPane {
     override lateinit var setPane: (Any) -> Unit
+    @FXML lateinit var titleImage: ImageView
     @FXML lateinit var canvas: Canvas
     private val model: TitleModel by lazy { TitleModel(canvas.graphicsContext2D) }
 
     override fun initialize(location: URL?, resources: ResourceBundle?) {
+        titleImage.image = "image/title.png".getResourceAsImage()
         model.draw()
     }
 
